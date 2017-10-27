0fd11c6b788ddc2353f3ffd5cf1e7abd5c35ca02
Hervé Yviquel <hyviquel@gmail.com>
2017-10-27 16:58:27 -0200

    add missing sudo to the setup script

--- cluster-mpi/setup-env.sh
+++ cluster-mpi/setup-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until sudo apt-get -y update && apt-get -y install openmpi-bin openmpi-common
+until sudo apt-get -y update && sudo apt-get -y install openmpi-bin openmpi-common
 do
  echo "Try again"
  sleep 2
